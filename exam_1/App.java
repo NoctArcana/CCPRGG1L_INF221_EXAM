58201a6d11b0b95badb6e6b3944beb7144536ff1
Biennico Ross Abitria <111473226+NoctArcana@users.noreply.github.com>
2022-09-05 09:42:05 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,20 @@
+public class App {
+   public static void main(String[] args) {
+
+    Student Me = new Student();
+
+    Me.Surname = "Abitria";
+    Me.FirstName = "Biennico";
+    Me.MiddleInitial = "A";
+    Me.DateOfBirth = 11/14/2003;
+    Me.StudentNumber = 2022101970;
+    Me.StudentEmailAddress = "abitriaba@students.national-u.edu.ph";
+    Me.IAmAwesome = true;
+    Me.sayMyStudentNumber();
+    Me.sayMyEmailAddress();
+    Me.amIAwesome();
+
+   }
+
+
+}
